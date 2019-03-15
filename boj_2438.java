858ca825c6662cc8d2ddf57a8cce9d5924a1de39
heeesun <34915280+heeesun@users.noreply.github.com>
2019-03-15 20:10:38 +0900

    BOJ 백준 2438 별찍기-1

---
+++ boj_2438.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/*
+https://www.acmicpc.net/problem/2438
+*/
+public class Main {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int num=in.nextInt();
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=i; j++) {
+			System.out.print("*");
+		}
+			System.out.println();
+	}
+	}
+}
