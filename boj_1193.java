54af333fa3192d2bb454dde39157f5c750834d83
heeesun <34915280+heeesun@users.noreply.github.com>
2019-03-15 17:37:42 +0900

    Create boj_1193.java

---
+++ boj_1193.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/*
+https://www.acmicpc.net/problem/1193
+*/
+public class Main {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int num=in.nextInt();
+		int tmp=0,divisor=0,dividend=0;
+		for(int i=1; i<10000; i++) {
+			if(num>tmp && tmp+i>=num) {
+				if(i%2==0) {
+					divisor=i+1-(num-tmp);
+					dividend=(num-tmp);	
+				}
+				else {
+					divisor=(num-tmp);
+					dividend=i+1-(num-tmp);
+				}
+				break;
+			}
+			tmp+=i;
+		}
+		System.out.print(dividend+"/"+divisor);
+	}
+}
